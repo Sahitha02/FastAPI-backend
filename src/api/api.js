21fdcb8b80b830a8772db23891fb98b024d736c8
Sahitha02 <sahithacheekatla02@gmail.com>
2025-12-04 17:07:15 +0530

    Add env and api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
 
 // --- Create axios instance ---
 const client = axios.create({
