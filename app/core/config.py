e2ca9cff4498fb873fcbf590444b2e1d12062e36
Sahitha02 <sahithacheekatla02@gmail.com>
2025-12-04 17:13:26 +0530

    Add env config and gitignore for deployment

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseSettings
 
-class Settings(BaseModel):
+class Settings(BaseSettings):
     PROJECT_NAME: str = "NavRoutes API"
-
-    DATABASE_URL: str = "postgresql+psycopg2://postgres:487755@localhost:5433/navroutes"
-
-    SECRET_KEY: str = "supersecretkey-change-this"
+    DATABASE_URL: str
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
+    class Config:
+        env_file = ".env"
+
 settings = Settings()
+
 
 
